65c9cfb96a9d443b053c0266577d48f70abf36c7
Charles H <chathaway@logrit.com>
2013-05-28 19:57:25 -0400

    Turns out there was an error

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
-this should cause an error
-
 setup(
     name='django-tastycake',
     version='1.0',
     author=u'Charles Hathaway',
     author_email='chathaway@logrit.com',
-    package_dir = {'':'src/sample_site/'}
+    package_dir = {'':'src/sample_site/'},
     packages=['tastycake'],
     url='https://github.com/chuck211991/django_tastycake',
     license='BSD license, see LICENCE.txt',
