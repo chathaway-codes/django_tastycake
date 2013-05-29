9f61ae13d53d6bf3313539c783b2996249573e43
Charles H <chathaway@logrit.com>
2013-05-28 20:29:05 -0400

    Added some dependencies

--- setup.py
+++ setup.py
@@ -9,6 +9,12 @@ setup(
     author_email='chathaway@logrit.com',
     package_dir = {'':'src/sample_site/'},
     packages=['tastycake'],
+    requires=[
+        'django_tastypie',
+    ],
+    install_require=[
+        'django_tastypie',
+    ],
     url='https://github.com/chuck211991/django_tastycake',
     license='BSD license, see LICENCE.txt',
     description='Adds some simplicity and compatibility to django-tastypie',
